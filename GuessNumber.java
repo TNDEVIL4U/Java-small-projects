28f4f3966176b3b20c8ef91c05ce68eaf98854e4
Barathi Shankar <barathishankarr@gmail.com>
2025-12-07 09:54:27 +0530

    Refactor GuessingGame to use threading and time tracking
    Refactored the guessing game to run in a separate thread, added time tracking for guesses, and improved user feedback.

--- GuessNumber.java
+++ GuessNumber.java
@@ -1,41 +1,51 @@
 //Program to create a random number generating game by giving clues to the user
-import java.util.Scanner;
-import java.util.Random;
-public class GuessingGame {
-public static void main(String[] args)
+import java.util.*;
+class Game implements Runnable
 {
-Random rand = new Random();
-int m,t=1;
-Scanner sc = new Scanner(System.in);
-boolean run = true;
-while(run){
-System.out.println("You have 10 chances");
-for(int i=0;i<10;i++)
-{
-int n = rand.nextInt(101);
-System.out.print("Guess the number (0-100) : ");
-m = sc.nextInt();
-if(n==m)
-{
-    System.out.println("Your Guess is correct");
-    break;
-}
-else if(m-10>n)
-    System.out.println("Your Guess value is too high");
+ public void run()
+ {
+     Random rand = new Random();
+     int m,t=1;
+     Scanner sc = new Scanner(System.in);
+     boolean run = true;
+     while(run){
+         int n = rand.nextInt(101);
+         System.out.println("You have 10 chances");
+         long start = System.currentTimeMillis();
+         for(int i=0;i<10;i++)
+         {
+             System.out.print("Guess the number (0-100) : ");
+             m = sc.nextInt();
+             if(n==m)
+             {
+                 long end = System.currentTimeMillis();
+                 System.out.println("Your Guess is correct\nTime Consumed : "+(end - start)/1000 + "secs ");
+                 break;
+             }
+             else if(m-10>n)
+                 System.out.println("Your Guess value is too high");
 
-else if(m+10<n) 
-    System.out.println("Your Guess value is too low");
-else if(m>n)
-    System.out.println("Your Guess value is little high");
-else
-    System.out.println("Your Guess value is little low");
-System.out.println("You have "+(9-i)+" Chances left");
+             else if(m+10<n)
+                 System.out.println("Your Guess value is too low");
+             else if(m>n)
+                 System.out.println("Your Guess value is little high");
+             else
+                 System.out.println("Your Guess value is little low");
+             System.out.println("You have "+(9-i)+" Chances left");
+         }
+         System.out.print("The answer is : "+n+"\nEnter 1 to start over or 0 to stop : ");
+         t = sc.nextInt();
+         if(t!=1)
+             run = false;
+     }
+ }
+}
+public class GuessNumber {
+    public static void main(String[] args)
+    {
+       Game g=new Game();
+       Thread t1=new Thread(g);
+       t1.start();
     }
-System.out.print("Enter 1 to start over or 0 to stop : ");
-t = sc.nextInt();
-if(t!=1)
- run = false;
 }
-sc.close();
-}
-}
+//COMPLETED
